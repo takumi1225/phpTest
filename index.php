68b3755078d53bfd57ea763e854e661d54674d0d
takumi1225 <takumi.a.tani@rakuten-sec.co.jp>
2021-08-24 10:07:12 +0900

    kadai

--- index.php
+++ index.php
@@ -42,3 +42,50 @@ $calendar_2020 = [
 echo $calendar_2020["December"];
 
 ?>
+
+<?php
+$height = 160;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+}
+
+$height = 160;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else {
+  echo "ご乗車になれます。";
+}
+
+$height = 230;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else if ($height >= 200){
+  echo "200cm 以上の方はご乗車できません。";
+} else {
+  echo ($height < 150) ? "150cm 未満の方はご乗車できません。" : "ご乗車になれます。";;
+}
+
+$weekday = "月曜";
+switch ($weekday) {
+  case "月曜":
+    echo "可燃ごみの日です。";
+    break;
+  case "水曜":
+    echo "資源ごみの日です。";
+    break;
+  default:
+    echo "回収はありません。";
+    break;
+}
+
+for($i = 0; $i < 10; $i++){
+  echo $i;
+}
+
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+}
+echo $total;
+?>
