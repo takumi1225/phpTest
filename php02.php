71da311c2a73a584c00fc13abe06fc711c95048d
takumi1225 <takumi.a.tani@rakuten-sec.co.jp>
2021-08-25 09:14:41 +0900

    課題を修正

---
+++ php02.php
@@ -0,0 +1,30 @@
+<?php
+//1. $name にあなたの名前を代入し、 if文で $name があなたの名前だったら 「私は あなたの名前 です」、もし違ったら「あなたの名前ではありません」と表示するように実装してください。
+$name = "Takumi";
+if ($name == "Takumi") {
+    echo "私はTakumiです";
+} else {  
+    echo "あなたの名前ではありません";
+}    
+//2. for文を使って、1から10000までの合計の値を表示してください。
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+  }
+  echo $total;
+//3. $fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください。
+$fruits = array("apple","banana","orange","grape","pineapple");
+foreach($fruits as $fruit) {
+    echo "フルーツは" . $fruit;
+    echo "\n";
+}
+//4.　【応用】 次のプログラムのバグを修正し、1から100までの間で5の倍数のみ表示するようにしてみてください。
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+  if($i % 5 == 0)
+    echo $i;
+}
+
+?> 
